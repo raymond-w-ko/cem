444190927bf0395510d3bb1bef8acdc7945c8fa8
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-02-15 16:12:58 -0500

    Add macros for enabling and disabling object bitfield options

--- .clj-kondo/macros.clj
+++ .clj-kondo/macros.clj
@@ -4,3 +4,11 @@
 (defn args [& xs]
   (let [vars (-> xs next next)]
     `(clojure.core/assoc ~'args :dummy (clojure.core/vector ~@vars))))
+
+(defmacro enable-obj-bitfield-option! [o field flag]
+  `(let [x# (. ~o ~field)]
+     (set! (. ~o ~field) (bit-or x# ~flag))))
+
+(defmacro disable-obj-bitfield-option! [o field flag]
+  `(let [x# (. ~o ~field)]
+     (set! (. ~o ~field) (bit-and x# (bit-not ~flag)))))
