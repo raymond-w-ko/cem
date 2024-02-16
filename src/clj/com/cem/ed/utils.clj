e2fd1077017059cf962a2e41ad816a5395abd0d6
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-02-16 01:24:59 -0500

    Add rand-int-between function to utils.clj

--- src/clj/com/cem/ed/utils.clj
+++ src/clj/com/cem/ed/utils.clj
@@ -4,3 +4,8 @@
 (defn get-timestamp []
   (let [now (Instant/now)]
     (.truncatedTo now (java.time.temporal.ChronoUnit/MILLIS))))
+
+(defn rand-int-between
+  "Returns a random integer between a and b, inclusive."
+  [a b]
+  (+ a (rand-int (- b a))))
