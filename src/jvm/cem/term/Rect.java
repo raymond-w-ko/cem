b78dfa92640edb2cdd2e4afd31a74f655542e3cf
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-03-02 11:15:56 -0500

    Rect: refactor, bug fixes

--- src/jvm/cem/term/Rect.java
+++ src/jvm/cem/term/Rect.java
@@ -16,27 +16,30 @@ public final class Rect {
     public int bgColor;
     public short bg_r, bg_g, bg_b, bg_a;
     public byte bold, italic, underline, dim, blink;
+    public Cell() {
+      // TODO: is this necessary?
+    }
   }
 
   public static final short selectUnsignedByteOfInt(int i, int n) {
     return (short)((i >> (n * 8)) & 0xFF);
   }
 
-  public static final void bytesToCell(int[] bytes, Cell cell) {
-    cell.codePoint = bytes[0];
-    final int fgColor = bytes[1];
+  public static final void bytesToCell(int[] cellBytes, Cell cell) {
+    cell.codePoint = cellBytes[0];
+    final int fgColor = cellBytes[1];
     cell.fgColor = fgColor;
     cell.fg_r = selectUnsignedByteOfInt(fgColor, 3);
     cell.fg_g = selectUnsignedByteOfInt(fgColor, 2);
     cell.fg_b = selectUnsignedByteOfInt(fgColor, 1);
     cell.fg_a = selectUnsignedByteOfInt(fgColor, 0);
-    final int bgColor = bytes[2];
+    final int bgColor = cellBytes[2];
     cell.bgColor = bgColor;
     cell.bg_r = selectUnsignedByteOfInt(bgColor, 3);
     cell.bg_g = selectUnsignedByteOfInt(bgColor, 2);
     cell.bg_b = selectUnsignedByteOfInt(bgColor, 1);
     cell.bg_a = selectUnsignedByteOfInt(bgColor, 0);
-    final int flags = bytes[3];
+    final int flags = cellBytes[3];
     cell.bold = (byte) (flags & Cell.FLAG_BOLD);
     cell.italic = (byte) (flags & Cell.FLAG_ITALIC);
     cell.underline = (byte) (flags & Cell.FLAG_UNDERLINE);
@@ -59,14 +62,15 @@ public final class Rect {
     numCols = -1;
     data = null;
     _tmpCellBytes = new int[getCellSizeAsNumInts()];
+    _tmpCell = new Cell();
   }
 
-  public final int rowColToIndex(final int row, final int col) {
+  public final int rowColToDataIndex(final int row, final int col) {
     assert row >= 0;
     assert col >= 0;
     assert row < this.numRows;
     assert col < this.numCols;
-    return row * numCols + col;
+    return (row * numCols + col) * getCellSizeAsNumInts();
   }
 
   public final void resize(final int _rows, final int _cols) {
@@ -84,7 +88,7 @@ public final class Rect {
   }
 
   public final Cell getCellIfDifferent(final int row, final int col, Rect other) {
-    final int i = rowColToIndex(row, col);
+    final int i = rowColToDataIndex(row, col);
     // a copy is necessary because this may be used in a multi-threaded context
     this._tmpCellBytes[0] = this.data[i];
     this._tmpCellBytes[1] = this.data[i + 1];
@@ -103,7 +107,7 @@ public final class Rect {
   }
 
   public final void updateBackingStore(final int row, final int col, Rect store) {
-    final int i = rowColToIndex(row, col);
+    final int i = rowColToDataIndex(row, col);
     store.data[i] = this._tmpCellBytes[0];
     store.data[i + 1] = this._tmpCellBytes[1];
     store.data[i + 2] = this._tmpCellBytes[2];
