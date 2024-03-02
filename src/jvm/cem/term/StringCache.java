ad4953cf143afeb867cc244b6b18abcef80ce137
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-03-01 23:49:23 -0500

    cem.term.StringCache: initial commit

---
+++ src/jvm/cem/term/StringCache.java
@@ -0,0 +1,17 @@
+package cem.term;
+
+public final class StringCache {
+  protected String[] cache;
+  public static final int MAX_CODE_POINT = 0x10FFFF;
+  public StringCache() {
+    cache = new String[MAX_CODE_POINT + 1];
+    for (int i = 0; i <= MAX_CODE_POINT; i++) {
+      final String x = new String(new int[] {i}, 0, 1);
+      final String s = x.intern();
+      cache[i] = s;
+    }
+  }
+  public String codePointToString(int codePoint) {
+    return cache[codePoint];
+  }
+}
