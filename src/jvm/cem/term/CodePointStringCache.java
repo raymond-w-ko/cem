faf9b5a530731e8d6d84a7537fcee3e319520c38
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-03-01 23:57:56 -0500

    rename file to CodePointStringCache.java

--- src/jvm/cem/term/CodePointStringCache.java
+++ src/jvm/cem/term/CodePointStringCache.java
@@ -1,9 +1,9 @@
 package cem.term;
 
-public final class StringCache {
+public final class CodePointStringCache {
   protected String[] cache;
   public static final int MAX_CODE_POINT = 0x10FFFF;
-  public StringCache() {
+  public CodePointStringCache() {
     cache = new String[MAX_CODE_POINT + 1];
     for (int i = 0; i <= MAX_CODE_POINT; i++) {
       final String x = new String(new int[] {i}, 0, 1);
