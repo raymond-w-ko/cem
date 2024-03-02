280254f6b65014a860426a68af90080c6d32789b
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-03-01 23:40:06 -0500

    cem.term.TermRenderState: initial commit

---
+++ src/jvm/cem/term/TermRenderState.java
@@ -0,0 +1,36 @@
+package cem.term;
+
+import java.lang.StringBuilder;
+
+public final class TermRenderState {
+  public int lastCol;
+  public int lastRow;
+
+  public byte lastDim;
+  public byte lastBold;
+  public byte lastBlink;
+
+  public byte lastItalic;
+
+  public byte lastUnderline;
+
+  public int lastFgColor;
+  public int lastBgColor;
+
+  public StringBuilder sb;
+
+  public TermRenderState() {
+    lastCol = Integer.MIN_VALUE;
+    lastRow = Integer.MIN_VALUE;
+    lastDim = -1;
+    lastBold = -1;
+    lastBlink = -1;
+    lastItalic = -1;
+    lastUnderline = -1;
+    // surely no one would use these colors with 0 alpha
+    lastFgColor = 0x11223300;
+    lastBgColor = 0x11223300;
+
+    sb = new StringBuilder();
+  }
+}
