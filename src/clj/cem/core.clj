4dda6f0067b610343772c1ce4a599de58e579342
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-03-01 23:40:33 -0500

    minor changes

--- src/clj/cem/core.clj
+++ src/clj/cem/core.clj
@@ -12,7 +12,8 @@
 (defn -main
   [& _args]
   (bb
-  ;;  (System/setProperty "clojure.core.async.go-checking" "true")
+   (System/setProperty "clojure.core.async.go-checking" "true")
+
    x (System/getenv "COLORTERM")
    :return-when (not x) (throw (new Exception "COLORTERM not set"))
    (when (and (not= x "truecolor") (not= x "24bit"))
@@ -24,7 +25,7 @@
                                         (System/exit 0))))
   (nrepl-server/start-server :port 6888 :handler cider-nrepl-handler)
   (term/init!)
-  (term/setup)
+  (term/setup!)
   (term/render-loop!)
   (<!! (term/stdin-read-loop!))
   (term/die-properly!))
